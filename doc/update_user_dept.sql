f7fbe4dd89a01601a2cf02a8ca7681da63fa8688
王志平 <461892464@qq.com>
2019-01-04 17:49:47 +0800

    用户所属商户的修改

--- doc/update_user_dept.sql
+++ doc/update_user_dept.sql
@@ -1,3 +1,5 @@
 update sys_user as u set u.dept_id = (select min( m.dept_id) from  merch_info m where m.merchno =u.username )
-where exists(select * from merch_info m where m.merchno = u.username)
+where exists(select * from merch_info m where m.merchno = u.username);
 
+
+update sys_user_role set role_id=11 where role_id=8
